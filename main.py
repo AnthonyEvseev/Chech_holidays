1136ad6b00622023eb22cab6019e9e440c555307
Anthony <tosharus26@gmail.com>
2022-05-24 18:27:48 +0300

    Отправляет запросы

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from datetime import date
 import asyncio
 from fast_bitrix24 import Bitrix
 
+calendar = ProdCalendar(locale='ru')
+
+
+async def main():
+    res = await calendar.month(date(2022, 5, 1), pre=True, locale='ru')
+    count = len([DateType.NOT_WORKING for day in res if res[day] == DateType.NOT_WORKING])
+    print(f'Days off in a month ', count)
+
+
 webhook = "https://b24-uxw5nw.bitrix24.ru/rest/1/jxcujeqn73soik0d/"
 b = Bitrix(webhook)
+deals = b.get_all('tasks.task.list')
 
-result = b.get_all('crm.lead.list')
+#task = {"ID": 1, 'fields': {"TITLE": 'task for test', 'RESPONSIBLE_ID': '1'}}
+#leads = b.call('tasks.task.add', task)
 
-# calendar = ProdCalendar(locale='ru')
-# async def main():
-#     res = await calendar.month(date(2022, 5, 1), pre=True, locale='ru')
-#     count = len([DateType.NOT_WORKING for day in res if res[day] == DateType.NOT_WORKING])
-#     print(f'Days off in a month ', count)
-#
-#
-# loop = asyncio.get_event_loop()
-# loop.create_task(main())
-# loop.run_forever()
+loop = asyncio.get_event_loop()
+loop.create_task(main())
+loop.run_forever()
