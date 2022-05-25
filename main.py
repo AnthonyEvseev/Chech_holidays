43fc1c6de801a11d82460709e9fb39dda36f14f3
Anthony <tosharus26@gmail.com>
2022-05-25 18:51:41 +0300

    Работает)

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 from fast_bitrix24 import Bitrix
-from datetime import date
+import datetime
 import holidays
 import schedule
-
-ru_holidays = holidays.RU()
-
-a = date(2022, 7, 1) in ru_holidays
-
-ru_holidays_name = list(ru_holidays.values())
-ru_holidays_data = list(ru_holidays.keys())
 
 webhook = "https://b24-uxw5nw.bitrix24.ru/rest/1/jxcujeqn73soik0d/"
 b = Bitrix(webhook)
 
+# Словарь праздников
+ru_holidays = holidays.RU()
 
-def append_task(name, data):
-    for item_name, item_data in zip(name, data):
-        task = {"ID": 1, 'fields': {"TITLE": f"{item_data} будет {item_name}", 'RESPONSIBLE_ID': '1'}}
+today = datetime.date.today()
+day_plus_3 = today + datetime.timedelta(days=3)
+check_holiday = today + datetime.timedelta(days=3) in ru_holidays
+
+ru_holidays_name = ru_holidays.get(day_plus_3)
+
+
+def task_holiday(today_check, day, name):
+    if today_check == True:
+        task = {"ID": 1, 'fields': {"TITLE": f"{day} будет {name}!\n"
+                                             f"Не забудь поздравить всех причастных)", 'RESPONSIBLE_ID': '1'}}
         b.call('tasks.task.add', task)
+    else:
+        print('Через 3 дня нет праздников\n'
+              'Засыпаю до завтра')
 
-
-append_task(ru_holidays_name, ru_holidays_data)
-
-
-def main():
-    schedule.every()
-
-# for item_name, item_data in zip(ru_holidays_name, ru_holidays_data):
-#     webhook = "https://b24-uxw5nw.bitrix24.ru/rest/1/jxcujeqn73soik0d/"
-#     b = Bitrix(webhook)
-#     task = {"ID": 1, 'fields': {"TITLE": f"{item_data} будет {item_name}", 'RESPONSIBLE_ID': '1'}}
-#     leads = b.call('tasks.task.add', task)
+schedule.every().day.at('10:00').do(task_holiday, check_holiday, day_plus_3, ru_holidays_name)
+while True:
+    schedule.run_pending()
