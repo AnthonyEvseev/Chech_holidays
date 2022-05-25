267a52a16dcc2eb2605ab28fe1c4e3b305a2f67e
Anthony <tosharus26@gmail.com>
2022-05-25 04:03:37 +0300

    Работаю над календарём

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from isdayoff import ProdCalendar, DateType
-from datetime import date
-import asyncio
 from fast_bitrix24 import Bitrix
 
-calendar = ProdCalendar(locale='ru')
+# webhook = "https://b24-uxw5nw.bitrix24.ru/rest/1/jxcujeqn73soik0d/"
+# b = Bitrix(webhook)
+# deals = b.get_all('tasks.task.list')
+
+# task = {"ID": 1, 'fields': {"TITLE": 'task for test', 'RESPONSIBLE_ID': '1'}}
+# leads = b.call('tasks.task.add', task)
+
+from datetime import date
+import holidays
+
+ru_holidays = holidays.RU()  # this is a dict
+
+a = date(2022, 7, 1) in ru_holidays  # True
+b = date(2022, 1, 2) in ru_holidays  # False
+c = ru_holidays.get('2014-01-01')
+d = ru_holidays.keys[1]
 
 
-async def main():
-    res = await calendar.month(date(2022, 5, 1), pre=True, locale='ru')
-    count = len([DateType.NOT_WORKING for day in res if res[day] == DateType.NOT_WORKING])
-    print(f'Days off in a month ', count)
 
 
-webhook = "https://b24-uxw5nw.bitrix24.ru/rest/1/jxcujeqn73soik0d/"
-b = Bitrix(webhook)
-deals = b.get_all('tasks.task.list')
-
-#task = {"ID": 1, 'fields': {"TITLE": 'task for test', 'RESPONSIBLE_ID': '1'}}
-#leads = b.call('tasks.task.add', task)
-
-loop = asyncio.get_event_loop()
-loop.create_task(main())
-loop.run_forever()
+print(f' Скоро {ru_holidays[2]}! Не забудь позравить всех причастных к празднику)')
+#print(f'{ru_holidays.keys()}')
+print(d)
