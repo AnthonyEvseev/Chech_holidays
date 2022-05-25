0148d0faa8e4fb1f8572b69f96242562ad6452be
Anthony <tosharus26@gmail.com>
2022-05-25 04:28:53 +0300

    Работаю над календарём

--- main.py
+++ main.py
@@ -10,16 +10,12 @@ from fast_bitrix24 import Bitrix
 from datetime import date
 import holidays
 
-ru_holidays = holidays.RU() # this is a dict
+ru_holidays = holidays.RU()  # this is a dict
 
-a = date(2022, 7, 1) in ru_holidays # True
-b = date(2022, 1, 2) in ru_holidays # False
-c = ru_holidays.get('2014-01-01')
-d = ru_holidays.keys[1]
+a = date(2022, 7, 1) in ru_holidays  # True
+b = date(2022, 1, 2) in ru_holidays  # False
+ru_holidays_name = list(ru_holidays.values())
+ru_holidays_data = list(ru_holidays.keys())
 
-
-
-
-print(f' Скоро {ru_holidays[2]}! Не забудь позравить всех причастных к празднику)')
-#print(f'{ru_holidays.keys()}')
-print(d)
+print(ru_holidays_name[10])
+print(ru_holidays_data[10])
